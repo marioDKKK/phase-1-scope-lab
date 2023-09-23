053be1598f984625455258157d67cf1b0ab023b6
mario <kinuthiamariodavis@gmail.com>
2023-09-23 15:21:04 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+// Write your solution in this file!
+var customerName="bob";
+const leastFavoriteCustomer = "least";
+
+function upperCaseCustomerName(){
+
+   
+    return  customerName.toUpperCase(); ;
+}
+
+console.log(upperCaseCustomerName());
+
+function setBestCustomer(customer ="not bob"){
+    bestCustomer = customer;
+    return bestCustomer;
+}
+
+function overwriteBestCustomer(customer){
+
+    return setBestCustomer(customer);
+}
+
+function changeLeastFavoriteCustomer(name){
+    leastFavoriteCustomer = name;
+    return leastFavoriteCustomer;
+}
